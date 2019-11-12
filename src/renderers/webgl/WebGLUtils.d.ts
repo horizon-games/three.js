abf04fa59857241353d27e4e880cefb63b6d1f75
Corban Riley <corbanbrook@gmail.com>
2019-11-12 12:10:55 -0500

    WebGL2RenderingContext not found.

--- src/renderers/webgl/WebGLUtils.d.ts
+++ src/renderers/webgl/WebGLUtils.d.ts
@@ -1,6 +1,6 @@
 export class WebGLUtils {
 
-	constructor( gl: WebGLRenderingContext | WebGL2RenderingContext, extensions: any, capabilities: any );
+	constructor( gl: WebGLRenderingContext, extensions: any, capabilities: any );
 
 	convert( p: any ): void;
 
