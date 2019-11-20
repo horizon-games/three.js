b2d465e1bf0a62881328e438745ce0d122d577eb
Tomasz Dysinski <td@horizongames.net>
2019-11-20 15:45:00 -0800

    added missing context restore method to types

--- src/renderers/WebGLRenderer.d.ts
+++ src/renderers/WebGLRenderer.d.ts
@@ -204,6 +204,7 @@ export class WebGLRenderer implements Renderer {
 	getContext(): WebGLRenderingContext;
 	getContextAttributes(): any;
 	forceContextLoss(): void;
+	forceContextRestore(): void;
 
 	/**
 	 * @deprecated Use {@link WebGLCapabilities#getMaxAnisotropy .capabilities.getMaxAnisotropy()} instead.
